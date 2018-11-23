26fe1038bae0bed125043f16205d1ee9dedb8b48
Dipak28 <39437666+Dipak28@users.noreply.github.com>
2018-11-23 19:15:44 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+import sklearn import SVC
+clf=svm.SVC(kernel="linear")
+clf.fit(features_train,labels_train)
+pred=clf.predict(feature_test)
+
+from sklearn.metrics import accuracy_score
+
+acc=accuracy_score(pred,label_test)
 
 #########################################################
 
